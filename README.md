88ad2b952f3f67d981867bf75a960d2d47f3f7ae
m <mcsar1997@gmail.com>
2020-01-19 17:34:58 -0200

    Implementa docker

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ O OneBitFlix é um sistema baseado no Netflix!
 Ele foi desenvolvido na 3ª edição da Semana Super Full Stack do OneBitCode.
 
 Nele é utilizado Ruby on Rails 5.2 com PostgreSQL no back-end e VueJS no front-end.
+
+## Diferenciais
+- Utilização do Docker
---
+++ start-dev.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+bundle check || bundle update
+
+yarn install
+>&2 echo "=========== YARN RUNNED"
+
+while ! pg_isready -h onebitflix-db -p 5432 -q -U postgres; do
+	>&2 echo "postgres is unavailable"
+	sleep 1
+done
+
+
+>&2 echo "postgres is up"
+
+if !(bundle exec rake db:migrate); then
+  >&2 echo "=========== DATABASE DOES NOT EXIST... yet"
+	bundle exec rake db:create
+	>&2 echo "=========== DATABASE CREATED"
+	bundle exec rake db:migrate
+	>&2 echo "=========== DATABASE MIGRATED"
+else
+  >&2 echo "=========== DATABASE EXIST.... yay"
+	bundle exec rake db:migrate
+	>&2 echo "=========== DATABASE MIGRATED (already exists)"
+fi
+
+bundle exec rake db:seed
+>&2 echo "=========== DATABASE SEEDED"
+
+bundle exec rails s -p 3000 -b '0.0.0.0'
