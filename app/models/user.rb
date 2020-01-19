209afa1aa1b9fa8517a5d62047a4e9bb1f6416e1
m <mcsar1997@gmail.com>
2020-01-19 20:15:19 -0200

    Adiciona relacionamento e validações

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
+
+  has_many :reviews
+  has_many :favorites
+  has_many :players
+  
+  validates :name, presence: true, on: :update
 end
