3a35a5fed504cd4b4ae6f4e2eb30f903702a5b79
m <mcsar1997@gmail.com>
2020-01-20 01:10:09 -0200

    Configura aws

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem "aws-sdk-s3"
+
 #Authentication
 gem 'devise'
 
--- app/serializers/api/v1/movie_serializer.rb
+++ app/serializers/api/v1/movie_serializer.rb
@@ -1,14 +1,12 @@
 class Api::V1::MovieSerializer
   include FastJsonapi::ObjectSerializer
-  
-  attributes :id, :title, :description, :episode_number, thumbnail_key,
-    :featured_thumbnail_key, :video_key, :serie_id
+  attributes :id, :title, :description, :episode_number, :serie_id
 
   attribute :type do |object|
     'serie'
   end
   
-  attribute :category do |object| 
+  attribute :category do |object|
     object.category&.name
   end
 
@@ -19,6 +17,24 @@ class Api::V1::MovieSerializer
   attribute :favorite do |object, params|
     if params.present? && params.has_key?(:user)
       params[:user].favorites.where(favoritable: object).exists?
+    end 
+  end
+
+  attribute :thumbnail_url do |object|
+    AWS_BUCKET.object("thumbnails/#{object.thumbnail_key}").presigned_url(:get, expires_in: 120)
+  end
+
+  attribute :thumbnail_cover_url do |object|
+    AWS_BUCKET.object("thumbnails/#{object.thumbnail_cover_key}").presigned_url(:get, expires_in: 120)
+  end
+
+  attribute :featured_thumbnail_url do |object|
+    if object[:featured_thumbnail_key].present?
+      AWS_BUCKET.object("thumbnails/#{object.featured_thumbnail_key}").presigned_url(:get, expires_in: 120)
     end
+  end
+
+  attribute :video_url do |object|
+    AWS_BUCKET.object("videos/#{object.video_key}").presigned_url(:get, expires_in: 120)
   end
 end
