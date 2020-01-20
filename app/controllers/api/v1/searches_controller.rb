1f60e389a036948c24f121c248c38dfbde69af2d
m <mcsar1997@gmail.com>
2020-01-20 00:19:43 -0200

    Adiciona corpo dos controllers

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::SearchesController < ApplicationController
   def index
+    check_search_value and (return if performed?)
+
+    render json: Api::V1::WatchableSerializer.new(
+      PgSearch.multisearch(params[:value]
+    ).order("created_at DESC").map(&:searchable)).serialized_json
+  end
+
+  private
+
+  def check_search_value
+    if params[:value].present? && params[:value].length < 3
+      render json: { errors: "Parameter :value must have at least 3 characters" }
+    end
   end
 end
